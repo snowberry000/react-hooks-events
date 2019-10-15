48bdafaa9ce87cd6e32756a4b21243ff2cb3e98f
snowberry000 <snowberry912@gmail.com>
2019-10-15 12:20:27 +0300

    add comment for sever environment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 require ('dotenv').config();
 
 let CONFIG = {};
-
+// process.env.STRIPE_CONNECT_CALLBACK_URL: https://app.heyagenda.com/settings
 CONFIG.REACT_APP_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/v1';
 CONFIG.STRIPE_CONNECT_CALLBACK_URL = process.env.STRIPE_CONNECT_CALLBACK_URL || 'http://localhost:3000/settings'
 module.exports = CONFIG;
