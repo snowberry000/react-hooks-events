9ced291124595bae3fdcd8c5ef169a435c2c11b8
snowberry000 <snowberry912@gmail.com>
2019-10-24 22:02:10 +0300

    add static redirect connect link

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ require ('dotenv').config();
 let CONFIG = {};
 // process.env.STRIPE_CONNECT_CALLBACK_URL: https://app.heyagenda.com/settings
 CONFIG.REACT_APP_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/v1';
-CONFIG.STRIPE_CONNECT_CALLBACK_URL = process.env.STRIPE_CONNECT_CALLBACK_URL || 'http://localhost:3000/settings'
+CONFIG.STRIPE_CONNECT_CALLBACK_URL = process.env.STRIPE_CONNECT_CALLBACK_URL || 'https://app.heyagenda.com/settings'
 module.exports = CONFIG;
