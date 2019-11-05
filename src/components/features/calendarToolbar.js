f5826605023ea56412ea995a3fc7d8ea16f07812
snowberry000 <snowberry912@gmail.com>
2019-11-05 08:39:44 +0200

    static view dropdown menu

--- src/components/features/calendarToolbar.js
+++ src/components/features/calendarToolbar.js
@@ -1,15 +1,21 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import moment from "moment";
 import H3 from "../typography/H3";
 import arrowRight from "../../images/ui/arrowRight.svg";
 import arrowLeft from "../../images/ui/arrowLeft.svg";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import SvgButton from "../buttons/SvgButton";
 import Button from "../buttons/Button";
 import ButtonsRow from "../layout/ButtonsRow";
 import expandGlyph from "../../images/Glyphs/calendarExpand.svg";
 import collapseGlyph from "../../images/Glyphs/calendarCollapse.svg";
 import CalendarContext from "../../contexts/CalendarContext";
+import PickerButton from "../buttons/PickerButton";
+import arrowDown from "../../images/ui/arrowDown.svg";
+import colors from "../style/colors.js";
+import CircleAddGlyph from "../../images/Glyphs/CircleAddGlyph";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Container = styled.div`
   display: flex;
@@ -26,6 +32,60 @@ const ResponsiveContainer = styled.div`
   }
 `;
 
+const loadingAni = keyframes`
+  50% {
+    opacity: .35;
+  }
+`;
+
+const ViewsButton = styled.div`
+  display: flex;
+  position: relative;
+`;
+
+const WrapperButton = styled(Button)`
+  p {
+    display: flex;
+  }
+`;
+
+const ViewDropDown = styled.div`
+  position: absolute;
+  right: 0;
+  top: 100%;
+  display: ${props => (props.isOpen? 'flex' : 'none')};
+  background-color: #f9f9f9;
+  color: ${colors.grey}
+  box-shadow: 0px 1.5px 1px rgba(0,0,0,0.05);
+  padding: 5px 10px;
+  border: 1px solid #E6E8E9;
+  border-radius: 0.25em;
+  z-index: 1;
+
+  ul {
+    margin: 0;
+    padding: 0;
+
+    li {
+      display: flex;
+      align-items: center;
+      list-style: none;
+      color: ${colors.grey};
+      white-space: nowrap;
+      font-size: 0.9rem;
+      font-weight: normal;
+      cursor: pointer;
+      margin: 0.3rem 0;
+    }
+  }
+`;
+
+const ViewTitle = styled.p`
+  font-size: 0.8rem;
+  color: ${colors.grey};
+  margin: 1rem 0 0.5rem;
+`;
+
 const CalendarToolbar = props => {
   const { date, view: currentView, views, onView, onNavigate } = props;
 
@@ -35,6 +95,11 @@ const CalendarToolbar = props => {
     if (currentView !== 'day')
       onView('day')
     onNavigate('TODAY')
+  }
+
+  const [openViewDropDown, setOpenViewDropDown] = useState(false);
+  const handleClickViews = () => {
+    setOpenViewDropDown(!openViewDropDown)
   }
 
   return (
@@ -91,6 +156,32 @@ const CalendarToolbar = props => {
           })}
         </ButtonsRow>
       </Container>
+      
+
+      <ViewsButton>
+        <WrapperButton onClick={handleClickViews}>
+          Views
+          <img
+            src={arrowDown}
+            alt="Show options"
+            style={{ transform: openViewDropDown ? "scaleY(-1)" : "", marginLeft: 5 }}
+          />
+        </WrapperButton>
+        <ViewDropDown isOpen={openViewDropDown}>
+          <ul>
+            <li>
+              <CircleAddGlyph side={16} fill={colors.grey} />&nbsp;&nbsp;Create another space
+            </li>
+            <ViewTitle>Views</ViewTitle>
+            <li>App Spaces</li>
+            <li>
+              <FontAwesomeIcon className="fa-icons" icon={faPencilAlt} />
+              &nbsp;&nbsp;Manage Views
+            </li>
+          </ul>
+        </ViewDropDown>
+      </ViewsButton>
+
     </Container>
   );
 };
